e2f3eb9095b099f339d8f574451aca19a58ae3cd
Ahmet-KARAGOZ <akaragoz5317@gmail.com>
2024-01-27 00:55:56 +0300

    26 Ocak 2024

---
+++ src/test/java/tests/day16_TestNG_POM/C06_ConfigurationPropertiesDosyasiKullanma.java
@@ -0,0 +1,30 @@
+package tests.day16_TestNG_POM;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+//1- https://www.testotomasyonu.com/ anasayfasina gidin
+//2- account linkine basin
+// 3- Kullanici email'i olarak “wise@gmail.com”girin
+// 4- Kullanici sifresi olarak “12345” girin
+// 5- Login butonuna basarak login olun
+// 6- Basarili olarak giris yapilabildigini test edin
+
+public class C06_ConfigurationPropertiesDosyasiKullanma {
+	@Test
+	public void pozitifLoginTesti(){
+		Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+		TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+		testOtomasyonPage.accountLinki.click();
+		testOtomasyonPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
+		testOtomasyonPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
+		testOtomasyonPage.loginButonu.click();
+		Assert.assertTrue(testOtomasyonPage.logoutButonu.isDisplayed());
+
+		//ReusableMethods.bekle(2);
+		Driver.quitDriver();
+	}
+}
