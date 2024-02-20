7ead00c6851cdd94d9f0ad980962aec9ab071cbb
Ahmet-KARAGOZ <akaragoz5317@gmail.com>
2024-02-20 19:53:46 +0300

    20 Subat

--- src/test/java/pages/JobExplorerProPage.java
+++ src/test/java/pages/JobExplorerProPage.java
@@ -19,65 +19,112 @@ public class JobExplorerProPage {
 
 	//--------- burdan sonraki locate ler kendi aldıklarım excel dosyasına eklenecek ---------
 
-	//US21 için kendi aldığım locateler
+	//US21 için  aldığım locateler Ahmet
 
-	@FindBy(xpath = "//h1[@class='page-title']") public WebElement adminDashboardPlatformStatisticsText;
-	@FindBy(xpath = "//*[text()='Last 24 hours']") public WebElement adminDashboardLast24HoursText;
-	@FindBy(xpath = "//*[text()='Payments']") public WebElement adminDashboardPaymentsText;
-	@FindBy(xpath = "//*[text()='Content']") public WebElement adminDashboardContentText;
-	@FindBy(xpath = "//*[text()='New users']") public WebElement adminDashboardNewUsersText;
-	@FindBy(xpath = "//*[text()='Users roles']") public WebElement adminDashboardUsersRolesText;
-	@FindBy(xpath = "//*[text()='Registered users']") public WebElement adminDashboardRegisteredUsersText;
-	@FindBy(xpath = "//input[@name='remember']") public WebElement adminDashboardBeniHatirlaCheckBox;
-	@FindBy(xpath = "//label[@for='remember']") public WebElement adminDashboardBeniHatirlaYazisi;
+	@FindBy(xpath = "//h1[@class='page-title']")
+	public WebElement adminDashboardPlatformStatisticsText; //Ahmet
+	@FindBy(xpath = "//*[text()='Last 24 hours']")
+	public WebElement adminDashboardLast24HoursText; //Ahmet
+	@FindBy(xpath = "//*[text()='Payments']")
+	public WebElement adminDashboardPaymentsText;//Ahmet
+	@FindBy(xpath = "//*[text()='Content']")
+	public WebElement adminDashboardContentText; //Ahmet
+	@FindBy(xpath = "//*[text()='New users']")
+	public WebElement adminDashboardNewUsersText;  //Ahmet
+	@FindBy(xpath = "//*[text()='Users roles']")
+	public WebElement adminDashboardUsersRolesText;  //Ahmet
+	@FindBy(xpath = "//*[text()='Registered users']")
+	public WebElement adminDashboardRegisteredUsersText;  //Ahmet
+	@FindBy(xpath = "//input[@name='remember']")
+	public WebElement adminDashboardBeniHatirlaCheckBox;  //Ahmet
+	@FindBy(xpath = "//label[@for='remember']")
+	public WebElement adminDashboardBeniHatirlaYazisi;  //Ahmet
 
 	//US21 için alınan locate lerin sonu
 
-	//US25 için olan locateler...
-	@FindBy(xpath = "//*[@class='hamburger btn-link']") public WebElement leftMenuButtonAdmin;
-	@FindBy(xpath = "//*[@href='#41-dropdown-element']") public WebElement leftMenuJobsButtonAdmin;
+	//US25 için ortak kullandığım locateler...
+	@FindBy(xpath = "//*[@class='hamburger btn-link']")
+	public WebElement leftMenuButtonAdmin;
+	@FindBy(xpath = "//*[@href='#41-dropdown-element']")
+	public WebElement leftMenuJobsButtonAdmin;
 
 	//US 25 Kendi aldığım locateler
-	@FindBy(xpath = "//span[@class='icon voyager-categories']") public WebElement leftMenuCategoriesIconuAdmin;
-	@FindBy(xpath = "//h1[@class='page-title']") public WebElement categoiresSayfaBasligiText;
-	@FindBy(xpath = "//a[@rel='next']") public WebElement categoriesNextPageIconu;
-	@FindBy(xpath = "//div[@class='show-res']") public WebElement categoriesSonucYazisi;
-
-	@FindBy(xpath = "//tbody/tr") public List<WebElement> categoriesTabloSatirlarListesi;
-	@FindBy(xpath = "//a[@class='btn btn-success btn-add-new']") public WebElement categoriesAddNewButton;
-	@FindBy(xpath = "//input[@name='name']") public WebElement addCategoryNameBox;
-	@FindBy(xpath = "//input[@name='slogan']") public WebElement addCategorySloganBox;
-	@FindBy(xpath = "//input[@name='description']") public WebElement addCategoryDescriptionBox;
-	@FindBy(xpath = "(//button[@type='submit'])[2]") public WebElement addCategorySaveButton;
-
-	@FindBy(xpath = "//tbody/tr/td[2]") public List<WebElement> categoriesTabloIsimList;
-	@FindBy(xpath = "(//div[@class='panel-body'])[5]") public WebElement viewingCategoryNameText;
-	@FindBy(xpath = "//input[@name='name']") public WebElement editCategoryNameBox;
-	@FindBy(xpath = "//input[@name='slogan']") public WebElement editCategorySloganBox;
-	@FindBy(xpath = "//input[@name='description']") public WebElement editCategoryDescritionBox;
-	@FindBy(xpath = "(//button[@type='submit'])[2]") public WebElement editCategorySaveButton;
-	@FindBy(xpath = "//input[@value='Yes, Delete it!']") public WebElement categorySilmePopupOnayButton;
+	@FindBy(xpath = "//span[@class='icon voyager-categories']")
+	public WebElement leftMenuCategoriesIconuAdmin;  //Ahmet
+	@FindBy(xpath = "//h1[@class='page-title']")
+	public WebElement categoiresSayfaBasligiText;  //Ahmet
+	@FindBy(xpath = "//a[@rel='next']")
+	public WebElement categoriesNextPageIconu;  //Ahmet
+	@FindBy(xpath = "//div[@class='show-res']")
+	public WebElement categoriesSonucYazisi;  //Ahmet
+	@FindBy(xpath = "//tbody/tr")
+	public List<WebElement> categoriesTabloSatirlarListesi; //Ahmet
+	@FindBy(xpath = "//a[@class='btn btn-success btn-add-new']")
+	public WebElement categoriesAddNewButton;  //Ahmet
+	@FindBy(xpath = "//input[@name='name']")
+	public WebElement addCategoryNameBox; //Ahmet
+	@FindBy(xpath = "//input[@name='slogan']")
+	public WebElement addCategorySloganBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='description']")
+	public WebElement addCategoryDescriptionBox;  //Ahmet
+	@FindBy(xpath = "(//button[@type='submit'])[2]")
+	public WebElement addCategorySaveButton; //Ahmet
+	@FindBy(xpath = "//tbody/tr/td[2]")
+	public List<WebElement> categoriesTabloIsimList;  //Ahmet
+	@FindBy(xpath = "(//div[@class='panel-body'])[5]")
+	public WebElement viewingCategoryNameText;  //Ahmet
+	@FindBy(xpath = "//input[@name='name']")
+	public WebElement editCategoryNameBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='slogan']")
+	public WebElement editCategorySloganBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='description']")
+	public WebElement editCategoryDescritionBox;  //Ahmet
+	@FindBy(xpath = "(//button[@type='submit'])[2]")
+	public WebElement editCategorySaveButton;  //Ahmet
+	@FindBy(xpath = "//input[@value='Yes, Delete it!']")
+	public WebElement categorySilmePopupOnayButton;  //Ahmet
 
 	//US31 için locateler
-	@FindBy(xpath = "//span[@class='icon voyager-company']") public WebElement leftMenuCompaniesButtonAdmin;
-	@FindBy(xpath = "//div[@class='show-res']") public WebElement companiesShowingEntryText;
-	@FindBy(xpath = "//tbody/tr") public List<WebElement> companiesTabloSatirlarList;
-	@FindBy(xpath = "//a[@rel='next']") public WebElement companiesNextPageButton;
-	@FindBy(xpath = "//a[@class='btn btn-success btn-add-new']") public WebElement companiesAddNewButonu;
-	@FindBy(xpath = "//input[@name='user_id']") public WebElement addCompanyUserIdBox;
-	@FindBy(xpath = "//input[@name='name']") public WebElement addCompanyNameBox;
-	@FindBy(xpath = "//input[@name='slug']") public WebElement addCompanySlugBox;
-	@FindBy(xpath = "//input[@name='hq']") public WebElement addCompanyHqBox;
-	@FindBy(xpath = "//input[@name='website_url']") public WebElement addCompanyWebUrlBox;
-	@FindBy(xpath = "//input[@name='email']") public WebElement addCompanyEmailBox;
-	@FindBy(xpath = "//input[@name='description']") public WebElement addCompanyDescriptionBox;
-	@FindBy(xpath = "(//button[@type='submit'])[2]") public WebElement addCompanySaveButton;
-	@FindBy(xpath = "//select[@id='search_key']") public WebElement companyAranacakBaslikDropdown;
-	@FindBy(xpath = "//select[@id='filter']") public WebElement companyAramaEsitlemeContainsDropDown;
-	@FindBy(xpath = "//input[@name='s']") public WebElement companyAramaSearchTextBox;
-	@FindBy(xpath = "//button[@class='btn btn-info btn-lg']") public WebElement companyAramaSearchIcon;
-	@FindBy(xpath = "//tbody/tr/td[4]") public List<WebElement> companiesNameList;
-	@FindBy(xpath = "//input[@value='Yes, Delete it!']") public WebElement companySilmePopupOnayButton;
+	@FindBy(xpath = "//span[@class='icon voyager-company']")
+	public WebElement leftMenuCompaniesButtonAdmin;  //Ahmet
+	@FindBy(xpath = "//div[@class='show-res']")
+	public WebElement companiesShowingEntryText; //Ahmet
+	@FindBy(xpath = "//tbody/tr")
+	public List<WebElement> companiesTabloSatirlarList; //Ahmet
+	@FindBy(xpath = "//a[@rel='next']")
+	public WebElement companiesNextPageButton;  //Ahmet
+	@FindBy(xpath = "//a[@class='btn btn-success btn-add-new']")
+	public WebElement companiesAddNewButonu;  //Ahmet
+	@FindBy(xpath = "//input[@name='user_id']")
+	public WebElement addCompanyUserIdBox; //Ahmet
+	@FindBy(xpath = "//input[@name='name']")
+	public WebElement addCompanyNameBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='slug']")
+	public WebElement addCompanySlugBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='hq']")
+	public WebElement addCompanyHqBox; //Ahmet
+	@FindBy(xpath = "//input[@name='website_url']")
+	public WebElement addCompanyWebUrlBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='email']")
+	public WebElement addCompanyEmailBox;  //Ahmet
+	@FindBy(xpath = "//input[@name='description']")
+	public WebElement addCompanyDescriptionBox;  //Ahmet
+	@FindBy(xpath = "(//button[@type='submit'])[2]")
+	public WebElement addCompanySaveButton;  //Ahmet
+	@FindBy(xpath = "//select[@id='search_key']")
+	public WebElement companyAranacakBaslikDropdown; //Ahmet
+	@FindBy(xpath = "//select[@id='filter']")
+	public WebElement companyAramaEsitlemeContainsDropDown;  //Ahmet
+	@FindBy(xpath = "//input[@name='s']")
+	public WebElement companyAramaSearchTextBox;  //Ahmet
+	@FindBy(xpath = "//button[@class='btn btn-info btn-lg']")
+	public WebElement companyAramaSearchIcon; //Ahmet
+	@FindBy(xpath = "//tbody/tr/td[4]")
+	public List<WebElement> companiesNameList;  //Ahmet
+	@FindBy(xpath = "//input[@value='Yes, Delete it!']")
+	public WebElement companySilmePopupOnayButton;  //Ahmet
+
+	//US_31 için locatelerin sonu
 
 
 
--- src/test/java/tests/myDay/US31_AdminCompanies.java
+++ src/test/java/tests/myDay/US31_AdminCompanies.java
@@ -25,7 +25,7 @@ eklediğim şirket bilgilerini düzenleyebilmeli.
 public class US31_AdminCompanies extends TestBaseRapor {
 	@Test
 	public void companiesUyeSirketleriGoruntulemeTesti_US31_TC01() {
-		extentTest = extentReports.createTest("Admin Sayfasi Categories Görüntüleme Testi", "Admin tüm kategorileri görüntüleyebilmeli.");
+		extentTest = extentReports.createTest("Admin Sayfasi Companies Görüntüleme Testi", "Admin tüm şirketleri görüntüleyebilmeli.");
 		JobExplorerProPage jobExplorerProPage = new JobExplorerProPage();
 		Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
 		extentTest.info("Jobexplorerpro Admin Login sayfasına gider.");
@@ -35,12 +35,13 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		extentTest.info("Geçerli email ve password bilgilerini girerek giriş butonuna tıklar.");
 
 		jobExplorerProPage.leftMenuButtonAdmin.click();
-		extentTest.info("Menü ikonu ile soldan açılır menüyü açar");
+		extentTest.info("Menü ikonu ile soldan açılır menüyü açar.");
 		jobExplorerProPage.leftMenuCompaniesButtonAdmin.click();
 
 		RecepHocaJSUtilities.scrollToBottom(Driver.getDriver());
 		String[] sonucYaziDizisi = jobExplorerProPage.companiesShowingEntryText.getText().split(" ");
 		List<WebElement> tabloToplamSatirlar = new ArrayList<>(jobExplorerProPage.companiesTabloSatirlarList);
+		extentTest.info("Sayfa sonuna gidip diğer sayfalarıda kontrol eder.");
 
 		while (!sonucYaziDizisi[3].equals(sonucYaziDizisi[5])) {
 			jobExplorerProPage.companiesNextPageButton.click();
@@ -50,13 +51,15 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		}
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertTrue(tabloToplamSatirlar.size() == Integer.parseInt(sonucYaziDizisi[3]));
+		extentTest.pass("Gösterilen sayıda Company olduğunu test eder.");
 		//System.out.println(tabloToplamSatirlar.size());
 		softAssert.assertAll();
+		extentTest.info("Sayfayı kapatır.");
 	}
 
 	@Test
 	public void companiesYeniSirketEkleyebilmeTesti_US31_TC02() {
-		extentTest = extentReports.createTest("Admin Sayfasi Categories Görüntüleme Testi", "Admin tüm kategorileri görüntüleyebilmeli.");
+		extentTest = extentReports.createTest("Admin Sayfasi Yeni Company Ekleyebilme Testi", "Admin yeni şirket ekleyebilmeli.");
 		JobExplorerProPage jobExplorerProPage = new JobExplorerProPage();
 		Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
 		extentTest.info("Jobexplorerpro Admin Login sayfasına gider.");
@@ -66,10 +69,11 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		extentTest.info("Geçerli email ve password bilgilerini girerek giriş butonuna tıklar.");
 
 		jobExplorerProPage.leftMenuButtonAdmin.click();
-		extentTest.info("Menü ikonu ile soldan açılır menüyü açar");
+		extentTest.info("Menü ikonu ile soldan açılır menüyü açar.");
 		jobExplorerProPage.leftMenuCompaniesButtonAdmin.click();
 
 		RecepHocaJSUtilities.clickWithJS(Driver.getDriver(), jobExplorerProPage.companiesAddNewButonu);
+		extentTest.info("Add New butonuna tıklar.");
 
 		//jobExplorerProPage.addCompanyUserIdBox.sendKeys(ConfigReader.getProperty("companyUserIdTest"));
 		//UserID girilince sayfa hata veriyor!-----------------------------------------------------------------
@@ -79,29 +83,39 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		jobExplorerProPage.addCompanyWebUrlBox.sendKeys(ConfigReader.getProperty("companyWebsiteUrlTest"));
 		jobExplorerProPage.addCompanyEmailBox.sendKeys(ConfigReader.getProperty("companyEmailTest"));
 		jobExplorerProPage.addCompanyDescriptionBox.sendKeys(ConfigReader.getProperty("companyDescriptionTest"));
+		extentTest.info("Name, Slug, Hq, WebsiteUrl, Email ve Description bilgilerini girer.");
 		jobExplorerProPage.addCompanySaveButton.click();
+		extentTest.info("Save butonuna tıklar.");
 
 		Select selectBaslik = new Select(jobExplorerProPage.companyAranacakBaslikDropdown);
 		selectBaslik.selectByValue("email");
+		extentTest.info("Arama kutusunda default User Id yazan dropdown menüden Email seçeneğini seçer.");
 
 		Select selectEsit = new Select(jobExplorerProPage.companyAramaEsitlemeContainsDropDown);
 		selectEsit.selectByValue("equals");
+		extentTest.info("Arama kutusunda default contains yazan dropdown menüden eşitlik simgesini seçer.");
 
 		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailTest"));
+		extentTest.info("Placeholderda Search yazan kutuya eklediği şirketin Email adresini yazar.");
 		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama mercek iconuna tıklar.");
 
 		List<String> isimlerStrList = new ArrayList<>(ReusableMethods.stringListeCevir(jobExplorerProPage.companiesNameList));
-		if (isimlerStrList.contains(ConfigReader.getProperty("companyNameTest"))) {
+		/*if (isimlerStrList.contains(ConfigReader.getProperty("companyNameTest"))) {
 			System.out.println("Company Ekleme BAşarılı...");
 		}
+
+		 */
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertTrue(isimlerStrList.contains(ConfigReader.getProperty("companyNameTest")), "Eklenen şirket bulunamadı!");
+		extentTest.pass("Sonuçlarda eklemiş olduğu şirketin göründüğünü test eder.");
 		softAssert.assertAll();
+		extentTest.info("Sayfayı kapatır.");
 	}
 
 	@Test(dependsOnMethods = "companiesYeniSirketEkleyebilmeTesti_US31_TC02")
 	public void eklenenSirketBilgileriniGuncellemeTesti_US31_TC03() {
-		extentTest = extentReports.createTest("Admin Sayfasi Categories Görüntüleme Testi", "Admin tüm kategorileri görüntüleyebilmeli.");
+		extentTest = extentReports.createTest("Admin Eklenen Company Düzenleme Testi", "Admin eklediği şirket bilgilerini güncelleyebilmeli.");
 		JobExplorerProPage jobExplorerProPage = new JobExplorerProPage();
 		Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
 		extentTest.info("Jobexplorerpro Admin Login sayfasına gider.");
@@ -111,28 +125,33 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		extentTest.info("Geçerli email ve password bilgilerini girerek giriş butonuna tıklar.");
 
 		jobExplorerProPage.leftMenuButtonAdmin.click();
-		extentTest.info("Menü ikonu ile soldan açılır menüyü açar");
+		extentTest.info("Menü ikonu ile soldan açılır menüyü açar.");
 		jobExplorerProPage.leftMenuCompaniesButtonAdmin.click();
 
 		Select selectBaslik = new Select(jobExplorerProPage.companyAranacakBaslikDropdown);
 		selectBaslik.selectByValue("email");
+		extentTest.info("Arama kutusunda default User Id yazan dropdown menüden Email seçeneğini seçer.");
 
 		Select selectEsit = new Select(jobExplorerProPage.companyAramaEsitlemeContainsDropDown);
 		selectEsit.selectByValue("equals");
+		extentTest.info("Arama kutusunda default contains yazan dropdown menüden eşitlik simgesini seçer.");
 
 		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailTest"));
+		extentTest.info("Placeholderda Search yazan kutuya eklediği şirketin Email adresini yazar.");
 		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama mercek iconuna tıklar.");
 
 		String dinamikNameXpath;
 		String dinamikXpathEditButton;
 
-		for (int i = 1; i <= jobExplorerProPage.categoriesTabloIsimList.size(); i++) {
+		for (int i = 1; i <= jobExplorerProPage.companiesTabloSatirlarList.size(); i++) {
 			dinamikNameXpath = "//tbody/tr[" + i + "]/td[4]";
 			dinamikXpathEditButton = "//tbody/tr[" + i + "]/td[8]/a[2]";
 			if (Driver.getDriver().findElement(By.xpath(dinamikNameXpath)).getText().equals(ConfigReader.getProperty("companyNameTest"))) {
 				Driver.getDriver().findElement(By.xpath(dinamikXpathEditButton)).click();
 			}
 		}
+		extentTest.info("Bulunan sonuçlarda eklemiş olduğu şirketin yanında bulunan Edit butonuna tıklar.");
 		//jobExplorerProPage.addCompanyUserIdBox.clear();
 		//jobExplorerProPage.addCompanyUserIdBox.sendKeys(ConfigReader.getProperty("companyUserIdEditTest"));
 		//UserID girilince sayfa hata veriyor!-----------------------------------------------------------------
@@ -148,13 +167,39 @@ public class US31_AdminCompanies extends TestBaseRapor {
 		jobExplorerProPage.addCompanyEmailBox.sendKeys(ConfigReader.getProperty("companyEmailEditTest"));
 		jobExplorerProPage.addCompanyDescriptionBox.clear();
 		jobExplorerProPage.addCompanyDescriptionBox.sendKeys(ConfigReader.getProperty("companyDescriptionEditTest"));
+		extentTest.info("Name, Slug, Hq, WebsiteUrl, Email ve Description bilgilerini günceller.");
 		jobExplorerProPage.addCompanySaveButton.click();
+		extentTest.info("Save butonuna tıklar.");
+
+		selectBaslik = new Select(jobExplorerProPage.companyAranacakBaslikDropdown);
+		selectBaslik.selectByValue("email");
+		extentTest.info("Arama kutusunda default User Id yazan dropdown menüden Email seçeneğini seçer.");
+
+		selectEsit = new Select(jobExplorerProPage.companyAramaEsitlemeContainsDropDown);
+		selectEsit.selectByValue("equals");
+		extentTest.info("Arama kutusunda default contains yazan dropdown menüden eşitlik simgesini seçer.");
+
+		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailEditTest"));
+		extentTest.info("Placeholderda Search yazan kutuya güncellediği şirket Email adresini yazar.");
+		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama mercek iconuna tıklar.");
+
+		List<String> isimlerStrList = new ArrayList<>(ReusableMethods.stringListeCevir(jobExplorerProPage.companiesNameList));
+		if (isimlerStrList.contains(ConfigReader.getProperty("companyEmailEditTest"))) {
+			System.out.println("Company Editleme Başarılı...");
+		}
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertTrue(isimlerStrList.contains(ConfigReader.getProperty("companyNameEditTest")), "Düzenlenen şirket bulunamadı!");
+		extentTest.pass("Sonuçlarda güncellenmiş şirket bilgilerinin göründüğünü test eder.");
+		softAssert.assertAll();
+		extentTest.info("Sayfayı kapatır.");
+
 
 	}
 
 	@Test(dependsOnMethods = "companiesYeniSirketEkleyebilmeTesti_US31_TC02")
 	public void istenenSirketBilgileriniSilmeTesti() {
-		extentTest = extentReports.createTest("Admin Sayfasi Categories Görüntüleme Testi", "Admin tüm kategorileri görüntüleyebilmeli.");
+		extentTest = extentReports.createTest("Admin Sayfasi Company Silme Testi", "Admin istediği şirketi silebilmeli.");
 		JobExplorerProPage jobExplorerProPage = new JobExplorerProPage();
 		Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
 		extentTest.info("Jobexplorerpro Admin Login sayfasına gider.");
@@ -169,12 +214,19 @@ public class US31_AdminCompanies extends TestBaseRapor {
 
 		Select selectBaslik = new Select(jobExplorerProPage.companyAranacakBaslikDropdown);
 		selectBaslik.selectByValue("email");
+		extentTest.info("Arama kutusunda default User Id yazan dropdown menüden Email seçeneğini seçer.");
 
 		Select selectEsit = new Select(jobExplorerProPage.companyAramaEsitlemeContainsDropDown);
 		selectEsit.selectByValue("equals");
+		extentTest.info("Arama kutusunda default contains yazan dropdown menüden eşitlik simgesini seçer.");
 
 		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailTest"));
+		extentTest.info("Placeholderda Search yazan kutuya silmek istediği şirketin Email adresini yazar.");
 		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama mercek iconuna tıklar.");
+
+		//Class olarak çalıştırıldığında eklenen şirket bilgilerinin güncellenmiş olabileceğinden dolayı
+		//Eğer normal email adresi ile kayıt bulamaz ise güncellenmiş şirket email adresi ile arama yapmak için yazılmıştır.
 		System.out.println(jobExplorerProPage.companiesShowingEntryText.getText().replaceAll("\\D", ""));
 		if (jobExplorerProPage.companiesShowingEntryText.getText().replaceAll("\\D", "").equals("0")) {
 			jobExplorerProPage.companyAramaSearchTextBox.clear();
@@ -190,31 +242,42 @@ public class US31_AdminCompanies extends TestBaseRapor {
 			dinamikXpathDeleteButton = "//tbody/tr[" + i + "]/td[8]/a[1]";
 			if (Driver.getDriver().findElement(By.xpath(dinamikNameXpath)).getText().equals(ConfigReader.getProperty("companyNameTest"))) {
 				Driver.getDriver().findElement(By.xpath(dinamikXpathDeleteButton)).click();
+				extentTest.info("Arama sonucunda çıkan şirket bilgilerinin yanında bulunan Delete butonuna tıklar.");
 			}
 			if (Driver.getDriver().findElement(By.xpath(dinamikNameXpath)).getText().equals(ConfigReader.getProperty("companyNameEditTest"))) {
 				Driver.getDriver().findElement(By.xpath(dinamikXpathDeleteButton)).click();
+				extentTest.info("Arama sonucunda çıkan şirket bilgilerinin yanında bulunan Delete butonuna tıklar.");
 			}
 
 		}
 		jobExplorerProPage.companySilmePopupOnayButton.click();
+		extentTest.info("Silme onayı için açılan Pop-up tan Yes Delete It seçeneğini seçer.");
 
 		selectBaslik = new Select(jobExplorerProPage.companyAranacakBaslikDropdown);
 		selectBaslik.selectByValue("email");
-
+		extentTest.info("Arama kutusunda default User Id yazan dropdown menüden Email seçeneğini seçer.");
 		selectEsit = new Select(jobExplorerProPage.companyAramaEsitlemeContainsDropDown);
 		selectEsit.selectByValue("equals");
-
+		extentTest.info("Arama kutusunda default contains yazan dropdown menüden eşitlik simgesini seçer.");
 		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailTest"));
+		extentTest.info("Placeholderda Search yazan kutuya sildiği şirketin Email adresini yazar.");
 		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama mercek iconuna tıklar.");
+
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertTrue(jobExplorerProPage.companiesShowingEntryText.getText().replaceAll("\\D", "").equals("0"));
+		extentTest.pass("Arama sonucunda sildiği şirketin görünmediğini test eder.");
 
 
 		jobExplorerProPage.companyAramaSearchTextBox.clear();
 		jobExplorerProPage.companyAramaSearchTextBox.sendKeys(ConfigReader.getProperty("companyEmailEditTest"));
 		jobExplorerProPage.companyAramaSearchIcon.click();
+		extentTest.info("Arama kutusu Email bölümüne editlenmiş şirket mail adresini girer.");
 		softAssert.assertTrue(jobExplorerProPage.companiesShowingEntryText.getText().replaceAll("\\D", "").equals("0"));
+		extentTest.pass("Arama sonucunda editlenmiş şirket bilgilerinin görünmediğini test eder.");
 		//softAssert.assertTrue(jobExplorerProPage.companiesNameList.isEmpty());
+		softAssert.assertAll();
+		extentTest.info("Sayfayı kapatır.");
 
 
 	}
